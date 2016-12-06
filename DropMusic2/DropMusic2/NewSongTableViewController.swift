9f142414d4ea52307d8e6aafd31cb78b7299a9f5
Angel Contreras <angel.contreras23@me.com>
2016-12-06 15:02:05 -0800

    Created NewSongTableController.swift

---
+++ DropMusic2/DropMusic2/NewSongTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NewSongTableViewController.swift
+//  DropMusic2
+//
+//  Created by Angel Contreras on 12/6/16.
+//  Copyright © 2016 Angel Contreras. All rights reserved.
+//
+
+import UIKit
+
+class NewSongTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+}
