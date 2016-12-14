b0a647fff664cce0d1b4f1f8b3ac7d1114c6b10f
Angel Contreras <angel.contreras23@me.com>
2016-12-13 20:08:01 -0800

    Fix album name bug

---
+++ DropMusic2/DropMusic2/PhoneUserController.swift
@@ -0,0 +1,10 @@
+//
+//  PhoneUserController.swift
+//  DropMusic2
+//
+//  Created by Angel Contreras on 12/12/16.
+//  Copyright © 2016 Angel Contreras. All rights reserved.
+//
+
+import Foundation
+
