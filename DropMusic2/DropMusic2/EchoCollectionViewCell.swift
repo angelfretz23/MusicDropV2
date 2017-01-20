febca135006136cf09af5e32727c14a5cdad3995
Angel Contreras <angel.contreras23@me.com>
2017-01-19 20:18:31 -0800

    Created Echo collectionviewcell class

---
+++ DropMusic2/DropMusic2/EchoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  EchoCollectionViewCell.swift
+//  DropMusic2
+//
+//  Created by Angel Contreras on 1/19/17.
+//  Copyright © 2017 Angel Contreras. All rights reserved.
+//
+
+import UIKit
+
+class EchoCollectionViewCell: UICollectionViewCell {
+    
+}
