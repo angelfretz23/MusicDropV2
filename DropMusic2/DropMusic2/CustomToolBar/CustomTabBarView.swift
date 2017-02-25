e6e99bef6c40faa6599e93a67361724f3f9304c8
Angel Contreras <angel@contrerasangel.com>
2017-02-24 16:41:05 -0800

    initialized UI elements

--- DropMusic2/DropMusic2/CustomToolBar/CustomTabBarView.swift
+++ DropMusic2/DropMusic2/CustomToolBar/CustomTabBarView.swift
@@ -14,9 +14,29 @@ class CustomTabBarView: UIView {
     var imageView1 = UIImageView()
     var imageView2 = UIImageView()
     var imageView3 = UIImageView()
+    var arrayOfImageViews = [UIImageView]()
     
     var button0 = UIButton()
     var button1 = UIButton()
     var button2 = UIButton()
+    var button3 = UIButton()
 
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        
+        button0.tag = 0
+        button1.tag = 1
+        button2.tag = 2
+        button3.tag = 3
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func selectIndex(index: Int){
+        //Set all to defaults (unselected)
+        imageView0.tintColor = UIColor.white
+        
+    }
 }
